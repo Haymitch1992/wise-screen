291c1b3132caab75220010faca2b61b9f02a4d77
003960 <wangzhiwei3306@163.com>
2021-05-12 14:55:08 +0800

    应急页面

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,7 @@ module.exports = {
   publicPath: '/',//vue-cli3.3+新版本使用
   devServer: {
       // 设置主机地址
-      host:"localhost",
-      // host: '192.168.60.74',
+
       // 设置默认端口
       port: 8080,
       open: false,//项目启动时是否自动打开浏览器，我这里设置为false,不打开，true表示打开
